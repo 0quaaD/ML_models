7234b05a1b8a3b815b506fb9e59ef7431077f4cb
0quaaD <eln6436@gmail.com>
2025-05-07 03:10:02 +0900

    Wine quality Classifier Model

---
+++ Model_files/wine_classifier_model.py
@@ -0,0 +1,61 @@
+## import numpy as np
+import pandas as pd
+import pickle
+import os 
+import warnings
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder, Binarizer
+from sklearn.ensemble import RandomForestClassifier
+
+
+warnings.filterwarnings('ignore')
+os.chdir('/home/elnokin/Desktop/files/Data Science/Pytorch-ML-AI/Datasets')
+print(os.getcwd())
+
+df = pd.read_csv('winequality-red.csv', on_bad_lines = 'warn')
+df = df.dropna()
+
+# there is no null values
+def plot_ph_sulphates_quality_relation(df):
+    sns.scatterplot(data = df, x='pH', y='quality')
+    plt.title('Relation of ph and Quality at Wine')
+    plt.show()
+    sns.scatterplot(data = df, y = 'sulphates', x = 'quality')
+    plt.title('Relation of sulphates and quality')
+    plt.show()
+
+#plot_ph_sulphates_quality_relation(df)
+
+corr_ = df.corr()
+#print(corr_[['quality']].sort_values(by='quality',ascending=False))
+X = df.drop(columns=['quality', 'free sulfur dioxide'],axis=1);
+
+# Binary classification: 6+ is good wine
+df['good'] = df['quality'] >= 6
+y1 = df['good'].astype(int)
+
+y = df['quality'].values
+one_hot = OneHotEncoder()
+label= LabelEncoder()
+binary = Binarizer(threshold=5.5)
+
+X = one_hot.fit_transform(X)
+y = binary.fit_transform(y.reshape(-1,1))
+
+train_X, test_X, train_y, test_y = train_test_split(X,y,test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=2000,max_depth=10000000, n_jobs=12, criterion='entropy')
+model.fit(train_X, train_y)
+
+y_pred = model.predict(test_X)
+
+acc = np.round(100 * accuracy_score(test_y, y_pred),2)
+print(f'Accuracy: {acc}%')
+
+with open('wine_model.pkl','wb') as file:
+    pickle.dump(model,file)
